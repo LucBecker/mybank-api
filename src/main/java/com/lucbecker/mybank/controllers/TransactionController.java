b7de98a633889a24d87da615e750c47caaee1be2
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-22 23:36:34 -0300

    wagger initiated and Controllers initiated

---
+++ src/main/java/com/lucbecker/mybank/controllers/TransactionController.java
@@ -0,0 +1,9 @@
+package com.lucbecker.mybank.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/transaction")
+public class TransactionController {
+}
